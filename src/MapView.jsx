7fcba834329a4f1b45b6e380534d1548f49da5ff
HenStei <68290173+HenStei@users.noreply.github.com>
2026-09-19 02:19:59 +0200

    fix: switch to esri map tiles to avoid api key requirement

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -233,10 +233,10 @@ export default function MapView() {
       
       <MapContainer center={userLocation} zoom={5} zoomControl={false} ref={setMap} className="w-full h-full z-0 bg-[#e5e5e5]">
         <ZoomControl position="bottomleft" />
-        {/* Moderne CartoDB Voyager Map Tiles anstatt Standard OSM */}
+        {/* Moderne Esri World Street Map (kostenlos, kein API Key nötig) */}
         <TileLayer 
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png" 
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}" 
+          attribution='Tiles &copy; Esri'
         />
         
         <MarkerClusterGroup chunkedLoading>
