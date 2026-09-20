58b66c3114f2004066d78181d273bb849e51ff29
HenStei <68290173+HenStei@users.noreply.github.com>
2026-09-20 21:30:03 +0200

    style: revert winter map to readable cartodb positron

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -833,8 +833,8 @@ export default function MapView() {
         )}
         {mapStyle === 'snow' && (
           <TileLayer 
-            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}" 
-            attribution='Tiles &copy; Esri'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" 
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             className="map-frozen"
           />
         )}
