89f59f2e48e10236cb4a0187dc4983209ac52270
HenStei <68290173+HenStei@users.noreply.github.com>
2026-09-22 19:35:50 +0200

    real-dark-mode-esri

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -1464,9 +1464,8 @@ const hasNightOwl = myPins.some(p => {
         )}
         {mapStyle === 'dark' && (
           <TileLayer 
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            className="map-dark"
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}" 
+            attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
           />
         )}
         {mapStyle === 'vintage' && (
