25d90fadc67703618b71015ed940a5c1d1aec8d4
HenStei <68290173+HenStei@users.noreply.github.com>
2026-09-22 19:31:27 +0200

    fix-dark-mode-api-key

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -1464,8 +1464,9 @@ const hasNightOwl = myPins.some(p => {
         )}
         {mapStyle === 'dark' && (
           <TileLayer 
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" 
-            attribution='&copy; CARTO'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            className="map-dark"
           />
         )}
         {mapStyle === 'vintage' && (
